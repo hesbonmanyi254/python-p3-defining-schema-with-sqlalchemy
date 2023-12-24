46d5347064322c653de5f52430a1fa382f9cb52e
hesbonmanyi254 <hesbonmanyinsa96@gmail.com>
2023-12-24 18:48:23 +0300

    Done lab

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_sandbox import Student
-
-engine = create_engine('sqlite:///students.db')
-
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+from sqlalchemy_sandbox import Student, Base
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,39 @@
-#!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
+# lib/sqlalchemy_sandbox.py
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
 if __name__ == '__main__':
-    pass
+    # Create a session
+    session = Session()
+
+    try:
+        # Update the existing student's name to "Geoffrey Nyanyuki"
+        existing_student = session.query(Student).filter_by(id=1).first()
+        if existing_student:
+            existing_student.name = 'Geoffrey Nyanyuki'
+            session.commit()
+
+        # Query all students
+        all_students = session.query(Student).all()
+        for student in all_students:
+            print(f"Student ID: {student.id}, Name: {student.name}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the session
+        session.close()
